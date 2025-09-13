c1f2d6839c6939349debfd2b172a1ec4f06a3f65
malsha7 <malsha74bopage@gmail.com>
2025-09-13 23:31:22 +0530

    Created RecipeViewModel

---
+++ EzyCook Recipe App/ViewModel/RecipeViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeViewModel.swift
+//  EzyCook Recipe App
+//
+//  Created by Malsha Bopage on 2025-09-13.
+//
+
+import Foundation
+import SwiftUI
+
+
+
+class RecipeViewModel: ObservableObject {
+    
+    
+    
+}
